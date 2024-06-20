0b7217a5e0f8716574873f03e3c962f57e44c257
Syed Faruque <talhafaruque6@gmail.com>
2024-06-20 01:51:51 -0400

    compactify

--- frontend/src/hooks/useSearchResults.jsx
+++ frontend/src/hooks/useSearchResults.jsx
@@ -1,36 +1,36 @@
 /**
- * @author Syed Faruque
+ * author: Syed Faruque
  * created: June 8 2024
 **/
 
-import {useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 
 const useSearchResults = (username, search, socket) => {
-    const [friends, setFriends] = useState([]);
-    const [nonfriends, setNonfriends] = useState([]);
-    const [requested, setRequested] = useState([]);
+  const [friends, setFriends] = useState([]);
+  const [nonfriends, setNonfriends] = useState([]);
+  const [requested, setRequested] = useState([]);
 
-    useEffect(() => {
-        if (!socket || !search || !username) return;
+  useEffect(() => {
+    if (!socket || !search || !username) return;
 
-        socket.emit("search", { search: search });
-        socket.on("update_search", () => {
-            socket.emit("search", { search: search });
-        });
+    socket.emit("search", { search: search });
+    socket.on("update_search", () => {
+      socket.emit("search", { search: search });
+    });
 
-        socket.on("search", (data) => {
-            setFriends(data.friends.filter(user => user !== username));
-            setNonfriends(data.nonfriends.filter(user => user !== username));
-            setRequested(data.requested.filter(user => user !== username));
-        });
+    socket.on("search", (data) => {
+      setFriends(data.friends.filter(user => user !== username));
+      setNonfriends(data.nonfriends.filter(user => user !== username));
+      setRequested(data.requested.filter(user => user !== username));
+    });
 
-        return () => {
-            socket.off("update_search");
-            socket.off("search");
-        };
-    }, [socket, search, username]);
-    
-    return {friends, nonfriends, requested};
-}
+    return () => {
+      socket.off("update_search");
+      socket.off("search");
+    };
+  }, [socket, search, username]);
+
+  return { friends, nonfriends, requested };
+};
 
 export default useSearchResults;
