6596e14641e7b96d1d29cfda6294ccbb9d038620
Syed Faruque <talhafaruque6@gmail.com>
2024-06-20 01:46:15 -0400

    compactify

--- frontend/src/game/GameBoard.jsx
+++ frontend/src/game/GameBoard.jsx
@@ -5,19 +5,19 @@ import useDisconnection from "../hooks/useDisconnection";
 import DisconnectMessage from "./DisconnectMessage";
 
 const GameBoard = ({ socket }) => {
-    const username = useAuth(socket);
-    const opponent = useOpponent(socket);
-    const disconnect = useDisconnection(socket);
+  const username = useAuth(socket);
+  const opponent = useOpponent(socket);
+  const disconnect = useDisconnection(socket);
 
-    return (
-        <div className="game-board">
-            {disconnect ? (
-                <DisconnectMessage opponent={opponent} socket={socket} />
-            ) : (
-                <p>Game Starts Here</p>
-            )}
-        </div>
-    );
+  return (
+    <div className="game-board">
+      {disconnect ? (
+        <DisconnectMessage opponent={opponent} socket={socket} />
+      ) : (
+        <p>Game Starts Here</p>
+      )}
+    </div>
+  );
 };
 
 export default GameBoard;
