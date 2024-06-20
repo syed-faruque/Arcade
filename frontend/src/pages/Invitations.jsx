0de746a755742a977adc7c673283fcb61fef2f28
Syed Faruque <talhafaruque6@gmail.com>
2024-06-20 01:53:38 -0400

    compactify

--- frontend/src/pages/Invitations.jsx
+++ frontend/src/pages/Invitations.jsx
@@ -1,5 +1,5 @@
 /**
- * @author Syed Faruque
+ * author: Syed Faruque
  * created: June 2 2024
 **/
 
@@ -9,30 +9,30 @@ import { useNavigate } from "react-router-dom";
 import useInvitations from "../hooks/useInvitations";
 
 const Invitations = ({ socket }) => {
-    const navigate = useNavigate();
-    const username = useAuth(socket);
-    const invites = useInvitations(username, socket);
+  const navigate = useNavigate();
+  const username = useAuth(socket);
+  const invites = useInvitations(username, socket);
 
-    const inviteMessages = invites.map(invite => invite.inviter + " challenges you to a duel");
+  const inviteMessages = invites.map(invite => invite.inviter + " challenges you to a duel");
 
-    const handleAccept = (index) => {
-        navigate(`/${invites[index].room_id}`);
-    };
+  const handleAccept = (index) => {
+    navigate(`/${invites[index].room_id}`);
+  };
 
-    const handleDecline = (index) => {
-        socket.emit("decline_invite", invites[index]);
-    };
+  const handleDecline = (index) => {
+    socket.emit("decline_invite", invites[index]);
+  };
 
-    return (
-        <ListWithActions
-            title="Invitations"
-            items={inviteMessages}
-            onAccept={handleAccept}
-            onDecline={handleDecline}
-            emptyMessage="No invitations found."
-            socket={socket}
-        />
-    );
+  return (
+    <ListWithActions
+      title="Invitations"
+      items={inviteMessages}
+      onAccept={handleAccept}
+      onDecline={handleDecline}
+      emptyMessage="No invitations found."
+      socket={socket}
+    />
+  );
 };
 
 export default Invitations;
