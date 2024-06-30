8ddd4a19808d7315f56cb8e64b30db8276832540
Syed Faruque <talhafaruque6@gmail.com>
2024-06-30 19:50:40 -0400

    improve spacing

--- backend/database.js
+++ backend/database.js
@@ -9,50 +9,50 @@ require('dotenv').config();
 
 // database connection
 const connection = sql.createConnection({
-    host: process.env.DATABASE_HOST, 
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD, 
-    database: process.env.DATABASE_NAME
+        host: process.env.DATABASE_HOST, 
+        user: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD, 
+        database: process.env.DATABASE_NAME
 });
 
 connection.connect(function(err) {
-    if (err) {
-        console.log("Could not establish database connection: " + err);
-        return;
-    }
-    console.log("Database connection established");
+        if (err) {
+                console.log("Could not establish database connection: " + err);
+                return;
+        }
+        console.log("Database connection established");
 });
 
 // accounts table setup
 connection.query(`
-    CREATE TABLE IF NOT EXISTS accounts (
-        username VARCHAR(255), 
-        password VARCHAR(255), 
-        online_status BOOLEAN
-    )
+        CREATE TABLE IF NOT EXISTS accounts (
+                username VARCHAR(255), 
+                password VARCHAR(255), 
+                online_status BOOLEAN
+        )
 `, function(error, results, fields) {
-    if (error) {
-        console.log("Error creating accounts table: " + error);
-    }
+        if (error) {
+                console.log("Error creating accounts table: " + error);
+        }
 });
 
 // socket_connections table setup
 connection.query(`
-    CREATE TABLE IF NOT EXISTS socket_connections (
-        username VARCHAR(255), 
-        socket_id VARCHAR(255)
-    )
+        CREATE TABLE IF NOT EXISTS socket_connections (
+                username VARCHAR(255), 
+                socket_id VARCHAR(255)
+        )
 `, function(error, results, fields) {
-    if (error) {
-        console.log("Error creating socket_connections table: " + error);
-    }
+        if (error) {
+                console.log("Error creating socket_connections table: " + error);
+        }
 });
 
 // friends table setup
 connection.query(`
-    CREATE TABLE IF NOT EXISTS friends (
-        user VARCHAR(255), 
-        friend VARCHAR(255)
+        CREATE TABLE IF NOT EXISTS friends (
+                user VARCHAR(255), 
+                friend VARCHAR(255)
     )
 `, function(error, results, fields) {
     if (error) {
@@ -62,43 +62,43 @@ connection.query(`
 
 // invites table setup
 connection.query(`
-    CREATE TABLE IF NOT EXISTS invites (
-        inviter VARCHAR(255), 
-        invited VARCHAR(255),
-        room_id VARCHAR(255)
-    )
+        CREATE TABLE IF NOT EXISTS invites (
+                inviter VARCHAR(255), 
+                invited VARCHAR(255),
+                room_id VARCHAR(255)
+        )
 `, function(error, results, fields) {
-    if (error) {
-        console.log("Error creating invites table: " + error);
-    }
+        if (error) {
+                console.log("Error creating invites table: " + error);
+        }
 });
 
 // friend requests table setup
 connection.query(`
-    CREATE TABLE IF NOT EXISTS friend_requests (
-        sender VARCHAR(255), 
-        receiver VARCHAR(255)
-    )
+        CREATE TABLE IF NOT EXISTS friend_requests (
+                sender VARCHAR(255), 
+                receiver VARCHAR(255)
+        )
 `, function(error, results, fields) {
-    if (error) {
-        console.log("Error creating friend_requests table: " + error);
-    }
+        if (error) {
+                console.log("Error creating friend_requests table: " + error);
+        }
 });
 
 // rooms table setup
 connection.query(`
-    CREATE TABLE IF NOT EXISTS rooms (
-        player1_socket_id VARCHAR(255), 
-        player1_username VARCHAR(255),
-        player2_socket_id VARCHAR(255), 
-        player2_username VARCHAR(255),
-        full BOOLEAN, 
-        room_id VARCHAR(255)
-    )
+        CREATE TABLE IF NOT EXISTS rooms (
+                player1_socket_id VARCHAR(255), 
+                player1_username VARCHAR(255),
+                player2_socket_id VARCHAR(255), 
+                player2_username VARCHAR(255),
+                full BOOLEAN, 
+                room_id VARCHAR(255)
+        )
 `, function(error, results, fields) {
-    if (error) {
-        console.log("Error creating rooms table: " + error);
-    }
+        if (error) {
+                console.log("Error creating rooms table: " + error);
+        }
 });
 
 module.exports = connection;
